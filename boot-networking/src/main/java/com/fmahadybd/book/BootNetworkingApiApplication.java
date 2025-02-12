f71e86e9de77061a5f8e311bf0a500004fe8b3fa
fmahadyBD <fmahady01@gmail.com>
2025-02-12 23:43:53 +0600

    Inital commit with setting up with docker compose

---
+++ boot-networking/src/main/java/com/fmahadybd/book/BootNetworkingApiApplication.java
@@ -0,0 +1,13 @@
+package com.fmahadybd.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootNetworkingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootNetworkingApiApplication.class, args);
+	}
+
+}
---
+++ boot-networking/src/test/java/com/fmahadybd/book/BootNetworkingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmahadybd.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootNetworkingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
