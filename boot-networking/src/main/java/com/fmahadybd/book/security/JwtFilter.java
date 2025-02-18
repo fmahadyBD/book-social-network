5969efca7254bf227f9822141671e6a1a52267c5
fmahadyBD <fmahady01@gmail.com>
2025-02-18 09:38:05 +0600

    working with security

---
+++ boot-networking/src/main/java/com/fmahadybd/book/security/JwtFilter.java
@@ -0,0 +1,8 @@
+package com.fmahadybd.book.security;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtFilter {
+    
+}
---
+++ boot-networking/src/main/java/com/fmahadybd/book/security/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.fmahadybd.book.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.AllArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+@EnableMethodSecurity(securedEnabled = true) // for role-based authentication
+public class SecurityConfig {
+
+    private final AuthenticationProvider authenticationProvider;
+    private JwtFilter jwtAuthFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(req -> req
+                        .requestMatchers(
+                                "/auth/**",
+                                "/v2/api-docs",
+                                "/v3/api-docs",
+                                "/swagger-resources",
+                                "/swagger-resources/**",
+                                "/configuration/ui",
+                                "/configuration/security",
+                                "/swagger-ui/**",
+                                "/webjars/**",
+                                "/swagger-ui.html")
+                        .permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS)) // Stateless session, it will
+                                                                                        // take every request as new
+                .authenticationProvider(authenticationProvider) // Use your authentication provider
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class); // Add JWT filter
+
+        return http.build();
+    }
+}
